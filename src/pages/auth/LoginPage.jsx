47e0c627c9475785f683ed80470d7d2703231e26
Van Hieu <hieuhk195@gmail.com>
2025-02-18 20:34:25 +0700

    handle endpoint login page

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,12 +1,58 @@
 import FormField from "@components/FormField";
 import TextInput from "@components/FormInput/TextInput";
-import { Box, Button, Link, Typography } from "@mui/material";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Alert, Box, Button, Link, Typography } from "@mui/material";
+import { openSnackbar } from "@redux/slices/snackBarSlice";
+import { useLoginMutation } from "@services/rootApi";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
 
 const LoginPage = () => {
-  const { control, handleSubmit } = useForm();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [login, { data, error, isSuccess, isError }] = useLoginMutation();
+
+  const onSubmit = (formDate) => {
+    login(formDate);
+  };
+
+  const formSchema = Yup.object().shape({
+    email: Yup.string()
+      .email()
+      .matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Invalid email format")
+      .required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  useEffect(() => {
+    if (isError) {
+      dispatch(
+        openSnackbar({
+          message: errors?.data?.message,
+          type: "error",
+        }),
+      );
+    }
+    if (isSuccess) {
+      dispatch(
+        openSnackbar({
+          message: data.message,
+        }),
+      );
+      navigate("/verify-otp");
+    }
+  }, [isSuccess, navigate, errors?.data?.message, isError, dispatch]);
   return (
     <div>
       <p className="mb-[6px] text-2xl text-dark-100">
@@ -15,15 +61,13 @@ const LoginPage = () => {
       <p className="mb-[26px] text-base text-dark-100">
         Please sign in to your account and start the adventure
       </p>
-      <form
-        className="gap-4"
-        onSubmit={handleSubmit((data) => console.log(data))}
-      >
+      <form className="gap-4" onSubmit={handleSubmit(onSubmit)}>
         <FormField
           control={control}
           label="Email"
           name="email"
           Component={TextInput}
+          error={errors.email}
         />
         <FormField
           control={control}
@@ -31,10 +75,14 @@ const LoginPage = () => {
           name="password"
           Component={TextInput}
           type="password"
+          error={errors.password}
         />
         <Button variant="contained" className="w-full" type="submit">
           Sign in
         </Button>
+        <Box className="mt-4">
+          {isError && <Alert severity="error">{error?.data?.message}</Alert>}
+        </Box>
         <Box className="mt-4">
           <Typography component="p">
             You do not have an account?{" "}
--- src/services/rootApi.js
+++ src/services/rootApi.js
@@ -1,21 +1,28 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const rootApi = createApi({
-    reducerPath: 'rootApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_BASE_URL
-    }), 
-    endpoints: (builder) => {
-        return {
-            register: builder.mutation({
-                query: ({fullName, email, password}) => ({
-                    url: '/signup',
-                    method: 'POST',
-                    body: {fullName, email, password}
-                })
-            })
-        }
-    },
-})
+  reducerPath: "rootApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+  }),
+  endpoints: (builder) => {
+    return {
+      register: builder.mutation({
+        query: ({ fullName, email, password }) => ({
+          url: "/signup",
+          method: "POST",
+          body: { fullName, email, password },
+        }),
+      }),
+      login: builder.mutation({
+        query: ({ email, password }) => ({
+          url: "/login",
+          method: "POST",
+          body: { email, password },
+        }),
+      }),
+    };
+  },
+});
 
-export const { useRegisterMutation } = rootApi
+export const { useRegisterMutation, useLoginMutation } = rootApi;
